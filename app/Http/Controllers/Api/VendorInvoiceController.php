7976237990fd842d683f74aaa53836696b57c94d
Ashok Nath <anath@awesomemotive.com>
2025-09-19 12:48:48 -0600

    Vendor file rename

--- app/Http/Controllers/Api/VendorInvoiceController.php
+++ app/Http/Controllers/Api/VendorInvoiceController.php
@@ -144,7 +144,10 @@ class VendorInvoiceController extends Controller
         // Handle file upload
         if ($request->hasFile('invoice_file')) {
             $file = $request->file('invoice_file');
-            $fileName = 'vendor_invoice_' . time() . '_' . $file->getClientOriginalName();
+            $vendor = Vendor::find($request->vendor_id);
+            $day = date('d', strtotime($request->invoice_date));
+            $vendorName = str_replace(' ', '_', $vendor->name);
+            $fileName = $day . '_' . $vendorName . '_' . time() . '_' . $file->getClientOriginalName();
             
             $googleDriveService = new GoogleDriveService();
             
@@ -223,7 +226,10 @@ class VendorInvoiceController extends Controller
         // Handle file upload
         if ($request->hasFile('invoice_file')) {
             $file = $request->file('invoice_file');
-            $fileName = 'vendor_invoice_' . time() . '_' . $file->getClientOriginalName();
+            $vendor = Vendor::find($request->vendor_id);
+            $day = date('d', strtotime($request->invoice_date));
+            $vendorName = str_replace(' ', '_', $vendor->name);
+            $fileName = $day . '_' . $vendorName . '_' . time() . '_' . $file->getClientOriginalName();
             
             $googleDriveService = new GoogleDriveService();
             
@@ -319,7 +325,10 @@ class VendorInvoiceController extends Controller
         // Handle file upload
         if ($request->hasFile('invoice_file')) {
             $file = $request->file('invoice_file');
-            $fileName = 'vendor_invoice_' . time() . '_' . $file->getClientOriginalName();
+            $vendor = Vendor::find($request->vendor_id);
+            $day = date('d', strtotime($request->invoice_date));
+            $vendorName = str_replace(' ', '_', $vendor->name);
+            $fileName = $day . '_' . $vendorName . '_' . time() . '_' . $file->getClientOriginalName();
             
             $googleDriveService = new GoogleDriveService();
             
